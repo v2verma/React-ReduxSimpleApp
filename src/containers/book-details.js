91cfc7e875a64d0d4513ef92620faf82b44b4a9e
Vivek Kumar Verma <v2verma@users.noreply.github.com>
2018-04-09 20:17:44 +0530

    initial change

--- src/containers/book-details.js
+++ src/containers/book-details.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class BookDetail extends Component {
-  render(){
-    if(!this.props.book){
-      return <div>Select a book to get started </div>
+  render() {
+    if (!this.props.book) {
+      return (
+        <div className="container" style={{ marginTop: "2%" }}>
+          <div className="row">
+            <div className="col-md-4">
+              Select a book to get started 
+            </div>
+          </div>
+        </div>
+        );
     }
-    return(
-      <div className="col-sm-8"><br />
-        <h4>Details for Book : </h4>
-        <div>Title : {this.props.book.title}</div>
-        <div>Pages : {this.props.book.pages}</div>
+    return (
+      <div className="container" style={{ marginTop: "2%" }}>
+        <div className="row">
+          <div className="col-md-4">
+            <br />
+            <h4>Details for Book : </h4>
+            <div>Title : {this.props.book.title}</div>
+            <div>Pages : {this.props.book.pages}</div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -19,7 +32,7 @@ class BookDetail extends Component {
 function mapStateToProps(state) {
   return {
     book: state.activeBook
-  }
+  };
 }
 
 export default connect(mapStateToProps)(BookDetail);

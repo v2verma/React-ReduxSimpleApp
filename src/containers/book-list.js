2629f60088613869b3405a452c7ee7e07c53fcce
Vivek Kumar Verma <v2verma@users.noreply.github.com>
2018-04-09 20:18:27 +0530

    changed the initial style

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,40 +1,44 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {selectBook} from "../actions/index";
-import {bindActionCreators} from "redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
 
 class BookList extends Component {
-
   renderList() {
-    return this.props.books.map((book)=>{
-      return(
-        <li className="list-group-item" 
-          onClick={()=> this.props.selectedBook(book)}>
+    return this.props.books.map(book => {
+      return (
+        <li
+          className="list-group-item"
+          onClick={() => this.props.selectedBook(book)}
+          style={{cursor: 'pointer'}}
+        >
           {book.title}
         </li>
-      )
-    })
+      );
+    });
   }
 
-  render(){
-    return(
-      <div className="col-sm-4">
-        <ol className="list-group">
-          {this.renderList()}
-        </ol>
+  render() {
+    return (
+      <div className="container" style={{ marginTop: "2%" }}>
+        <div className="row">
+          <div className="col-md-4">
+            <ol className="list-group">{this.renderList()}</ol>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return{
+  return {
     books: state.books
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({selectedBook : selectBook}, dispatch);
+  return bindActionCreators({ selectedBook: selectBook }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);

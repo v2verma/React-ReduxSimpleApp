63db2d372b4851238bd320d9d07a9e0988bdb2df
Vivek Kumar Verma <v2verma@users.noreply.github.com>
2018-04-09 20:16:36 +0530

    changed the styling

--- src/components/app.js
+++ src/components/app.js
@@ -6,13 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default class App extends Component {
   render(){
     return(
-      <div className="row">
-        <div className="container">
-          <div className="col-sm-12">
-          <BookList />
-          <BookDetail />
-        </div>
-        </div>
+      <div>
+        <BookList />
+        <BookDetail />
       </div>
     );
   }
